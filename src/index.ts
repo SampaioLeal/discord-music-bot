53f25e294f0d5d670755fa9fb8cb94c4211f96d8
Sampaio Leal <samp14hd@gmail.com>
2021-01-10 17:51:36 -0300

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+interface GenericType {
+  A: string;
+  B: number;
+}
+
+const object: GenericType = {
+  A: 'foo',
+  B: 2,
+};
+
+console.log(object);
+console.log('Start developing your NodeTS application');
